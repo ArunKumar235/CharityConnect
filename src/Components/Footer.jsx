401cbdc447fd84e36a6d09f049e93113494c6e33
Arun Kumar M <marunkumar0203@gmail.com>
2025-02-27 12:58:23 +0530

    Integrated Boostrap

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div className="container">
+            <footer className="py-3 mt-4">
+                <p className="text-center text-body-secondary">© 2024 Company, Inc</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
